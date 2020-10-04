502f45021c74f99314b61e06a1c1afa78e00a687
anthonybaezlt@gmail.com <tonyko94>
2020-10-04 18:09:32 -0500

    Realizando arrayz para mostrar cursos mdiante map con sus propiedades

--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,36 +1,37 @@
 import React from "react"
-
-const Curso = () =>(
-
+import PropTypes from 'prop-types'
 
 
-    
+const Curso =({title, image, price, profesor}) =>(
+
     <article className="card">
       <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-        <img src="https://edteam-media.s3.amazonaws.com/courses/small/91e149d0-961a-4594-a8ff-0a625be9cdd2.png" alt="Poster Curso"/>
+        <img src={image} alt={title}/>
       </div>
+
       <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-        <h3 className="t5 s-mb-2 s-center">
-          Programación orientada a objetos con Go
-        </h3>
-        <div className="s-mb-2 s-main-center">
-          <div className="card__teacher s-cross-center">
-            <div className="card__avatar s-mr-1">
-              <div className="circle img-container">
-                <img src="https://edteam-media.s3.amazonaws.com/users/thumbnail/66f015b2-0dfb-4ba9-bd0d-f7a7e1855275.jpeg" alt="Tio alexys"/>
-              </div>
-            </div>
-            <span className="small">Alexys Lozada</span>
-          </div>
-        </div>
+        <h3 className="center">{title}</h3>
         <div className="s-main-center">
-          <a className="button--ghost-alert button--tiny" href="#">$ 20USD</a>
+            {`Prof.: ${profesor}`}
+        </div>
+        <div className="s-mb-2 s-main-center">
+            <a className="button--ghost-alert button--tiny" href="#">{`$ ${price}`}</a>
         </div>
       </div>
     </article>
-
-
-
 )
 
+Curso.propTypes = {
+    title: PropTypes.string,
+    image: PropTypes.string,
+    price: PropTypes.number,
+    profesor: PropTypes.string
+  }
+  
+  Curso.defaultProps ={
+    title: "No se encontró título",
+    image: "https://cache.desktopnexus.com/thumbseg/1726/1726524-bigthumbnail.jpg",
+    price:"--",
+    profesor:""
+  }
 export default Curso
