e6bade469e426bce7905d819e531997ca2f3db47
anthonybaezlt@gmail.com <tonyko94>
2020-10-05 00:16:43 -0500

    Creando carpetas history, mainmenu, course y coursegrip e importandolas, dando click a menu y tarjetas te reedirecciona a descripcion por medio de links

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import "./styles/styles.scss"
-import Curso from "./Curso"
+// import Curso from "./Curso"
 import Banner from "./Banner"
 import Form from "./Form"
-
+import CourseGrid from './CourseGrid'
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import Course from "./Course"
+import MainMenu from './MainMenu';
+import History from "./History"
 
 
 
 const App = () => (
-  <>
-<Banner/>
-<Form />
-
-
-</>
+  <Router>
+    <MainMenu />
+    <Switch>
+    <Route path="/" exact component= {Banner} />
+    <Route path="/cursos/:id"  component={ Course}/>
+    <Route path="/cursos"  component= {CourseGrid} />
+    <Route path="/history"  component={ History}/>
+    <Route path="/form" component={ () => <Form name = " Página de contacto"/>} />
+    <Route component={() =>(
+        <div className="ed-grid">
+          <h1>Error 404</h1>
+          <span>Página no encontrada</span>
+        </div>
+   )}/>
+    </Switch>
+  </Router>
 )
 
 export default App;
---
+++ src/History.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const History = ({match, location, history}) => (
+    <div>
+        {JSON.stringify(match)}
+        </div>
+)
+
+
+export default History
